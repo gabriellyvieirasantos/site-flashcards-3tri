5511cf1dbe2cb5cd91e210c61f2d043197eba186
gabriellyvieirasantos <140664240+gabriellyvieirasantos@users.noreply.github.com>
2024-11-20 21:50:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Historia',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o ponto mais alto do planeta?',
+    'Monte Everest.'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual é o valor de π (pi) até a segunda casa decimal?',
+    '3,14'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o órgão responsável pela produção de insulina no corpo humano?',
+    'Pâncreas'
+)
+criaCartao(
+    'Português',
+    'O que é uma oração subordinada?',
+    'Uma oração que depende de outra para ter sentido completo'
+)
+criaCartao(
+    'Fisica',
+    'O que é a lei da inércia?',
+    'A tendência de um corpo a manter seu estado de movimento ou repouso'
+)
